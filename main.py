e93f294d797ab7907e26cbf80f600dc0c3c688b8
HuchKnows <47354545+HuchKnows@users.noreply.github.com>
2021-08-10 10:21:32 -0400

    major and minor scales created, needs error handling and input verification

--- main.py
+++ main.py
@@ -5,25 +5,30 @@ from Note import Note
 from Degree import Degree
 from Scale import Scale
 
+search_key = input("Input key: ")
+
 notes = ['C', 'Db', 'D', 'Eb', 'E', 'F', 'Gb', 'G', 'Ab', 'A', 'Bb', 'B']
 degree_symbols = ['I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX', 'X', 'XI', 'XII']
 
-C = Note(notes[0], notes[0])
-Db = Note(notes[1], notes[1])
-D = Note(notes[2], notes[2])
-Eb = Note(notes[3], notes[3])
-E = Note(notes[4], notes[4])
-F = Note(notes[5], notes[5])
-Gb = Note(notes[6], notes[6])
-G = Note(notes[7], notes[7])
-Ab = Note(notes[8], notes[8])
-A = Note(notes[9], notes[9])
-Bb = Note(notes[10], notes[10])
-B = Note(notes[11], notes[11])
 
-notes = [C, Db, D, Eb, E, F, Gb, G, Ab, A, Bb, B]
+def note_builder(note_list):
+    """creates all of the note objects"""
+    C = Note(note_list[0], note_list[0])
+    Db = Note(note_list[1], note_list[1])
+    D = Note(note_list[2], note_list[2])
+    Eb = Note(note_list[3], note_list[3])
+    E = Note(note_list[4], note_list[4])
+    F = Note(note_list[5], note_list[5])
+    Gb = Note(note_list[6], note_list[6])
+    G = Note(note_list[7], note_list[7])
+    Ab = Note(note_list[8], note_list[8])
+    A = Note(note_list[9], note_list[9])
+    Bb = Note(note_list[10], note_list[10])
+    B = Note(note_list[11], note_list[11])
+    return [C, Db, D, Eb, E, F, Gb, G, Ab, A, Bb, B]
 
-search_key = input("Input key: ")
+
+notes = note_builder(notes)
 
 
 def organize_list(key):
@@ -34,58 +39,66 @@ def organize_list(key):
             length = len(notes)
             index = notes.index(note)
             new_list = [note]
-            for i in range(index + 1, length):
-                new_list.append(notes[i])
+            for x in range(index + 1, length):
+                new_list.append(notes[x])
             for j in range(0, index):
                 new_list.append(notes[j])
             return new_list
 
 
-I = Degree('First', degree_symbols[0])
-II = Degree('Second', degree_symbols[1])
-III = Degree('Third', degree_symbols[2])
-IV = Degree('Fourth', degree_symbols[3])
-V = Degree('Fifth', degree_symbols[4])
-VI = Degree('Sixth', degree_symbols[5])
-VII = Degree('Seventh', degree_symbols[6])
-VIII = Degree('Eighth', degree_symbols[7])
-IX = Degree('Ninth', degree_symbols[8])
-X = Degree('Tenth', degree_symbols[9])
-XI = Degree('Eleventh', degree_symbols[10])
-XII = Degree('Twelfth', degree_symbols[11])
+def degree_builder():
+    """creates enough Degree objects to cover the total number of Notes"""
+    I = Degree('First', degree_symbols[0])
+    II = Degree('Second', degree_symbols[1])
+    III = Degree('Third', degree_symbols[2])
+    IV = Degree('Fourth', degree_symbols[3])
+    V = Degree('Fifth', degree_symbols[4])
+    VI = Degree('Sixth', degree_symbols[5])
+    VII = Degree('Seventh', degree_symbols[6])
+    VIII = Degree('Eighth', degree_symbols[7])
+    IX = Degree('Ninth', degree_symbols[8])
+    X = Degree('Tenth', degree_symbols[9])
+    XI = Degree('Eleventh', degree_symbols[10])
+    XII = Degree('Twelfth', degree_symbols[11])
+    return [I, II, III, IV, V, VI, VII, VIII, IX, X, XI, XII]
 
 
-# degrees = [I, II, III, IV, V, VI, VII, VIII, IX, X, XI, XII]
+degrees = degree_builder()
+
 
 # MAKE MAJOR SCALE PATTERN
-def major_scale(notes):
+def major_scale(note_list, degree_list):
     """assigns correct notes to scale degrees"""
-    major_scale_degrees = [I, II, III, IV, V, VI, VII]
-    major_scale_pattern = [notes[0], notes[2], notes[4], notes[5],
-                           notes[7], notes[9], notes[11]]
+    major_scale_degrees = [degree_list[0], degree_list[1], degree_list[2], degree_list[3],
+                           degree_list[4], degree_list[5], degree_list[6]]
+    major_scale_pattern = [note_list[0], note_list[2], note_list[4], note_list[5],
+                           note_list[7], note_list[9], note_list[11]]
     for k in range(0, len(major_scale_pattern)):
         major_scale_degrees[k].assign_note(major_scale_pattern[k])
-    major_scale = Scale(f"{I.note.name} Major Scale", I.note, major_scale_degrees)
-    return major_scale
+    return Scale(f"{major_scale_degrees[0].note.name} Major Scale",
+                 major_scale_degrees[0].note,
+                 major_scale_degrees)
 
 
-def minor_scale(notes):
+def minor_scale(note_list, degree_list):
     """assigns correct notes to scale degrees for minor scale"""
-    minor_scale_degrees = [I, II, III, IV, V, VI, VII]
-    minor_scale_pattern = [notes[0], notes[2], notes[3], notes[5],
-                           notes[7], notes[8], notes[10]]
+    minor_scale_degrees = [degree_list[0], degree_list[1], degree_list[2], degree_list[3],
+                           degree_list[4], degree_list[5], degree_list[6]]
+    minor_scale_pattern = [note_list[0], note_list[2], note_list[3], note_list[5],
+                           note_list[7], note_list[8], note_list[10]]
     for k in range(0, len(minor_scale_pattern)):
         minor_scale_degrees[k].assign_note(minor_scale_pattern[k])
-    minor_scale = Scale(f"{I.note.name} Minor Scale", I.note, minor_scale_degrees)
-    return minor_scale
+    return Scale(f"{minor_scale_degrees[0].note.name} Minor Scale",
+                 minor_scale_degrees[0].note,
+                 minor_scale_degrees)
 
 
-major_scale = major_scale(organize_list(search_key))
+major_scale = major_scale(organize_list(search_key), degrees)
 print(major_scale.name)
 for i in range(0, len(major_scale.degrees)):
     print(f"{major_scale.degrees[i].symbol}: {major_scale.degrees[i].note.name}")
 
-minor_scale = minor_scale(organize_list(search_key))
+minor_scale = minor_scale(organize_list(search_key), degrees)
 print(minor_scale.name)
 for i in range(0, len(minor_scale.degrees)):
     print(f"{minor_scale.degrees[i].symbol}: {minor_scale.degrees[i].note.name}")
