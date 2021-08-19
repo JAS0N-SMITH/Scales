4d211b07094ca2d6a7c1efb5d7a3d0a9db363d72
HuchKnows <47354545+HuchKnows@users.noreply.github.com>
2021-08-19 09:31:01 -0400

    Updated program to be more modular, added a dictionary to store scale info, updated readme

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ MAIN
 from Note import Note
 from Degree import Degree
 from Scale import Scale
+from Scale_Options import scale_options_dict
 
 search_key = input("Input key: ")
 
@@ -65,52 +66,6 @@ def degree_builder():
 
 degrees = degree_builder()
 
-scale_options_dict = {
-    'Major Scale': {
-        'scale_degrees': 7,
-        'interval_pattern': [0, 2, 4, 5, 7, 9, 11],
-    },
-    'Natural Minor Scale': {
-        'scale_degrees': 7,
-        'interval_pattern': [0, 2, 3, 5, 7, 8, 10],
-    },
-    'Major Pentatonic Scale': {
-        'scale_degrees': 5,
-        'interval_pattern': [0, 2, 4, 7, 9],
-    },
-    'Minor Pentatonic Scale': {
-        'scale_degrees': 5,
-        'interval_pattern': [0, 3, 5, 7, 10],
-    },
-    'Blues Scale': {
-        'scale_degrees': 6,
-        'interval_pattern': [0, 3, 5, 6, 7, 10],
-    },
-    'Mixolydian Mode': {
-        'scale_degrees': 7,
-        'interval_pattern': [0, 2, 4, 5, 7, 9, 10],
-    },
-    'Lydian Mode': {
-        'scale_degrees': 7,
-        'interval_pattern': [0, 2, 4, 6, 7, 9, 11],
-    },
-    'Locrian Mode': {
-        'scale_degrees': 7,
-        'interval_pattern': [0, 1, 3, 5, 6, 8, 10],
-    },
-    'Dorian Mode': {
-        'scale_degrees': 7,
-        'interval_pattern': [0, 2, 3, 5, 7, 9, 10],
-    },
-    'Phrygian Mode': {
-        'scale_degrees': 7,
-        'interval_pattern': [0, 1, 3, 5, 7, 8, 10],
-    },
-    'Phrygian Dominant Scale': {
-        'scale_degrees': 7,
-        'interval_pattern': [0, 1, 4, 5, 7, 8, 10],
-    }
-}
 
 # mps = scale_options_dict.get('Minor Pentatonic Scale')
 # print(scale_options_dict.keys())
