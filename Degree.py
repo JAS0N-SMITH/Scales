eec531fad8128e3501595d593ef8dbf45d3785c2
Jason Smith <47354545+JAS0N-SMITH@users.noreply.github.com>
2023-08-28 14:10:07 +0000

    Added document comments to Degree.py and Note.py

--- Degree.py
+++ Degree.py
@@ -6,9 +6,11 @@ how many? C,Db,D,Eb,E,F,Gb,G,Ab,A,Bb,B
 
 
 class Degree:
+    """name: the name of the degree, symbol: the symbol of the degree"""
     def __init__(self, name, symbol):
         self.name = name
         self.symbol = symbol
 
     def assign_note(self, note):
+        """assigns a note to the degree"""
         self.note = note
--- Note.py
+++ Note.py
@@ -4,6 +4,7 @@ THE NOTE CLASS
 
 
 class Note:
+    """name: the name of the note, key: the key of the note"""
     def __init__(self, name, key):
         self.name = name
         self.key = key
